5bac8e2626709bc0fb8b57758924aec392664db6
BenSoft <eternelle.vision@outlook.fr>
2023-04-16 22:48:49 +0100

    Template String.....

---
+++ chapter01_syntaxe_basics_types/03_template_string.js
@@ -0,0 +1,9 @@
+const ytbName = "Bensoft";
+const author = "Ben Araham";
+const totalVideos = 50;
+
+
+//Template string
+let fullText = `Bienvenue ma chaine ${ytbName}  animé par ${author}, avec ${totalVideos} abonés.`;
+
+console.log(fullText);
